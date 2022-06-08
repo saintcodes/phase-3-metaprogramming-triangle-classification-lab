c8c0358291020065e1a002f61f596858980636b4
Michelle <clairsin@gmail.com>
2022-06-08 17:40:07 -0400

    commit tree 2852c499aa6330468dd4798952ce6a3f4b127fe8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end 
+
+  def kind
+    if a == b && a == c
+      :equilateral
+    elsif (a == b || a == c || b == c)
+      :isosceles
+    elsif a != b && a != c
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    puts "error"
+    # def message
+    #   if (a<=0 || b<=0 || c<=0)
+    #     "Error"
+    #   elsif ((a + b < c) || (a + c < b) || (b + c < a))
+    #     "Error"
+    #   end
+    # end
+  end
+
 end
